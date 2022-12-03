1eef6df3cfc2e0c4f37aa696288f06d0e9a9dd59
Jarasłaŭ Viktorčyk <ugzuzg@gmail.com>
2022-12-03 12:08:28 +0100

    update deck setup

--- deck/setup.sh
+++ deck/setup.sh
@@ -1,5 +1,38 @@
 #!/usr/bin/bash -xe
 
+# Install system packages
+sudo steamos-readonly disable
+sudo pacman-key --init
+sudo pacman-key --populate archlinux
+sudo pacman-key --populate holo
+
+sudo sed -i -E 's/\[(jupiter|holo|core|extra|community|multilib)\]/[\1-beta]/g' /etc/pacman.conf
+
+if ! grep -qe "heftig" /etc/pacman.conf; then
+  cat <<EOF | sudo tee -a /etc/pacman.conf
+
+[heftig]
+SigLevel = Optional
+Server = https://pkgbuild.com/~heftig/repo/\$arch
+EOF
+fi
+
+paru -Sy --noconfirm archlinux-keyring
+paru -S --noconfirm openssl
+
+paru -S --noconfirm --needed \
+    zsh zoxide bat tmux base-devel exa \
+    rustup \
+    lib32-freetype2 \
+    fakeroot p7zip unrar \
+    steam-boilr-gui antigen \
+    neovim dotter-rs-bin \
+    firefox-nightly
+
+sudo usermod -s /bin/zsh deck
+
+#sudo steamos-readonly enable
+
 # Install flatpaks
 flatpak install --noninteractive --or-update flathub \
     `# Apps` \
@@ -11,33 +44,3 @@ flatpak install --noninteractive --or-update flathub \
     org.gnome.Platform.Compat.i386//43
 flatpak install --noninteractive --or-update flathub-beta \
     net.lutris.Lutris
-
-# Install system packages
-sudo steamos-readonly disable
-sudo pacman-key --init
-sudo pacman-key --populate archlinux
-
-if ! grep -qe "heftig" /etc/pacman.conf; then
-  cat <<EOF | sudo tee -a /etc/pacman.conf
-
-[heftig]
-SigLevel = Optional
-Server = https://pkgbuild.com/~heftig/repo/\$arch
-EOF
-fi
-
-# reinstall openssl
-# rust fails to build things otherwise
-yay -Sy --noconfirm --overwrite '*' openssl
-yay -Sy --noconfirm --needed --overwrite '*' \
-    zsh zoxide bat tmux base-devel exa \
-    rustup \
-    lib32-freetype2 \
-    fakeroot p7zip unrar \
-    steam-boilr-gui itch-bin antigen \
-    neovim dotter-rs-bin \
-    firefox-nightly
-
-sudo usermod -s /bin/zsh deck
-
-# sudo steamos-readonly enable
